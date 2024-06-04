3f522b7c05f46dd34493d2224cc02623a5885c10
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2024-06-04 02:51:01 -0500

    Logica de ceacion usuarios
    Se ha creado la logica de creacion de usuarios
    
    Co-Authored-By: PaulaGiraldo <79341465+PaulaGiraldo@users.noreply.github.com>

--- app/src/main/java/com/equipouno/app/view/fragment/RegisterFragment.kt
+++ app/src/main/java/com/equipouno/app/view/fragment/RegisterFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.equipouno.app.databinding.FragmentRegisterBinding
 import androidx.navigation.fragment.findNavController
 import com.equipouno.app.R
+import com.equipouno.app.model.User
+import com.equipouno.app.viewmodel.LoginModel
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
+    private val loginViewModel: LoginModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +29,40 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.registerButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val phone = binding.phoneEditText.text.toString()
+            val address = binding.addressEditText.text.toString()
+            val birthdate = binding.birthdateEditText.text.toString()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || phone.isEmpty() || address.isEmpty() || birthdate.isEmpty()) {
+                Toast.makeText(context, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val newUser = User(
+                id = "",
+                name = name,
+                password = password,
+                tel = phone,
+                address = address,
+                birthdate = birthdate,
+                email = email
+            )
+
+            loginViewModel.registerUser(newUser){ isCreated ->
+                if (isCreated){
+                    Toast.makeText( context, "Se creo correctamente", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_registerFragment_to_nav_home)
+                }else {
+                    Toast.makeText( context, "Ups!!, paso un error al crear el usuario", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
 
         binding.loginButtonRegister.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_nav_home)
