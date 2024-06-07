383abe67b3da1217d4235168ea0a9f904a1b1640
Isabela_ <isabela.rosero@correounivalle.edu.co>
2024-06-06 23:36:57 -0500

    Delivery y mejoramiento de RecipesDetails

--- app/src/main/java/com/equipouno/app/view/fragment/DeleveryFragment.kt
+++ app/src/main/java/com/equipouno/app/view/fragment/DeleveryFragment.kt
@@ -5,23 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.equipouno.app.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.equipouno.app.databinding.FragmentDeleveryBinding
-import com.equipouno.app.databinding.FragmentTypesFoodBinding
+import com.equipouno.app.viewmodel.UserModel
 
 class DeleveryFragment : Fragment() {
     private lateinit var binding: FragmentDeleveryBinding
+    private val userViewModel: UserModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?
-    {
+    ): View? {
         binding = FragmentDeleveryBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Supongamos que tienes el email del usuario de alguna manera
+        val userEmail = "user@example.com"
+
+        // Observa los datos del usuario
+        userViewModel.getUserByEmail(userEmail).observe(viewLifecycleOwner, Observer { user ->
+            user?.let {
+                binding.fieldNameValue.text = it.name
+                binding.fieldAddressValue.text = it.address
+                binding.fieldPhoneValue.text = it.tel
+            }
+        })
     }
 }
