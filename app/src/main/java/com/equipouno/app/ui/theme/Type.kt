cd70966d5097f6d06d2ba267412a5fd569294b80
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2024-06-06 21:57:38 -0500

    front

--- app/src/main/java/com/equipouno/app/ui/theme/Type.kt
+++ app/src/main/java/com/equipouno/app/ui/theme/Type.kt
@@ -3,8 +3,13 @@ package com.equipouno.app.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
+val ConcertOne = FontFamily(
+        Font(R.font.concertone, FontWeight.Normal, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
