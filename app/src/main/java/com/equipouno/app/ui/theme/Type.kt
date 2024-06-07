6c5d46a20bc67ea1e46fcaad25c9a88f10cbb55d
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2024-06-07 12:17:15 -0500

    Fixed mergue home
    Fixed mergue main
    
    Co-Authored-By: PaulaGiraldo <79341465+PaulaGiraldo@users.noreply.github.com>

--- app/src/main/java/com/equipouno/app/ui/theme/Type.kt
+++ app/src/main/java/com/equipouno/app/ui/theme/Type.kt
@@ -2,10 +2,12 @@ package com.equipouno.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.equipouno.app.R
 
 val ConcertOne = FontFamily(
         Font(R.font.concertone, FontWeight.Normal, FontStyle.Normal)
